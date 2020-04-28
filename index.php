87e10246291e8722855abcea0928a990a260e71d
deepak <dk122809@gmail.com>
2020-04-28 22:57:20 +0530

    some major changes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
   <meta name="description" content="Get the covid19 latest reports">
   <meta name="keywords" content="covid19, coronavirus, corona, virus, coronavirus report in india, covid19 report of india, total coronavirus cases in india">
+  <meta name="google-site-verification" content="VZitcEmUzuiOIfawfGPlS7WIIpsyEAa9igtIz2ZnlcM" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://kit.fontawesome.com/fa9b59c9b0.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
