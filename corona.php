0d5a66fd2cc6885809fa5d21cd1001e445f5e5b9
deepak <dk122809@gmail.com>
2020-04-30 18:22:38 +0530

    intial deploy

--- corona.php
+++ corona.php
@@ -1,45 +1,4 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Corona Tracker</title>
-		<meta charset="UTF-8">
-  <meta name="description" content="Get the covid19 latest reports">
-  <meta name="keywords" content="covid19, coronavirus, corona, virus, coronavirus report in india, covid19 report of india, total coronavirus cases in india">
-  <meta name="google-site-verification" content="VZitcEmUzuiOIfawfGPlS7WIIpsyEAa9igtIz2ZnlcM" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script src="https://kit.fontawesome.com/fa9b59c9b0.js" crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://projects.nodetent.com/coronatracker/assets/css/dataTables.bootstrap4.min.css">
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<script data-ad-client="ca-pub-9482927953483307" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	</head>
-	<body onload="fetch()">
-		<!-- header Start -->
-		
-<nav class="navbar navbar-expand-lg navbar-dark">
-   <a class="navbar-brand" href="index.php" style="margin-right: 68px;"><img src="images/logo.png" ></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-    	<li class="nav-item ">
-    		<a class="india" href="corona.php">India Stats</a>
-    	</li>
-		<li class="nav-item ">
-			<a href="india_hospital.php">Hospitals in India</a>
-			</li>
-		<li class="nav-item ">
-			<a href="helpline.php">Helpline No.</a>
-			</li>
-		<li class="nav-item ">
-			<a href="notification.php">Notification From Indian Goverment</a>
-		</li>
-		</ul>
-  </div>
-</nav>
-		<!-- header Ends -->
+<?php include 'include/india_header.php'; ?>
 
 		<div class="main-body">
 			<h1 align="center">India</h1><br>
---
+++ include/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Corona Tracker</title>
+	<meta charset="UTF-8">
+  <meta name="description" content="Get the covid19 latest reports">
+  <meta name="keywords" content="covid19, coronavirus, corona, virus, coronavirus report in india, covid19 report of india, total coronavirus cases in india">
+  <meta name="google-site-verification" content="VZitcEmUzuiOIfawfGPlS7WIIpsyEAa9igtIz2ZnlcM" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://kit.fontawesome.com/fa9b59c9b0.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://projects.nodetent.com/coronatracker/assets/css/dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<script data-ad-client="ca-pub-9482927953483307" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
+<body onload="fetch()">
